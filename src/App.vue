b2d7026990c36762d08ca5543b99e03b6c988b68
v_haojzhang <v_haojzhang@tencent.com>
2018-03-26 18:33:15 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <router-link tag="div" to="/index" class="to-index">
+        <i>Wepost</i>
+      </router-link>
+      <div class="linkbox">
+        <router-link tag="div" to="/index" class="link">
+          <span>Wepost</span>
+        </router-link>
+        <router-link tag="div" to="/index" class="link">
+          <span>Wepost</span>
+        </router-link>
+        <router-link tag="div" to="/index" class="link">
+          <span>Wepost</span>
+        </router-link>
+      </div>
+
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style  scoped lang="less" rel="stylesheet/less">
+  @import "./assets/less/index.less";
+  @media screen and (min-width:960px) and (max-width:1200px){
+    .to-index{
+      font-size: @font-size-md-5!important;
+    }
+  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  .header{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height:5rem;
+    line-height: 5rem;
+    background-color: aliceblue;
+    //color: #fff;
+
+    .to-index{
+      font-size: @font-size-large-5;
+      margin-left: 5rem;
+    }
+    .linkbox{
+      display: flex;
+      flex-direction: row;
+      justify-content: left;
+      .link{
+        font-size: @font-size-large-4;
+        margin: 0 3rem;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="index">index
+    </div>
+</template>
+<script type="text/ecmascript-6">
+    export default {
+        data() {
+            return {}
+        },
+        components: {},
+        methods: {},
+        watch: {},
+        computed: {},
+        created() {
+        },
+        mounted() {
+        }
+    }
+</script>
+<style scoped lang="less" rel="stylesheet/less">
+    @import "index.less";
+</style>
